0b639477608d8d416af52821ca7bed1bfd6a8439
Alex <anascimento@dev-id.fr>
2026-09-24 10:33:45 +0200

    notebook(Notebook): Add notebook and initial exploration

---
+++ main.py
@@ -0,0 +1,6 @@
+﻿from pathlib import Path
+
+path = Path(r"C:\Users\seugn\PycharmProjects\cas_usage\notebooks\notebook.ipynb")
+
+content = path.read_text(encoding="utf-8-sig")
+path.write_text(content, encoding="utf-8")
